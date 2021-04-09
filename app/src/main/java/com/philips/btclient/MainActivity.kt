92e4f20b21196ba65d48b8382cad1d500ab7f568
Martijn van Welie <martijn.van.welie@gmail.com>
2021-04-09 17:46:00 +0200

    First commit

---
+++ app/src/main/java/com/philips/btclient/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.philips.btclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
