16ce787391aedfc2d970c59eb20182c144a29fee
Abdul Nabi <abdul@codekrafters.com>
2022-12-07 18:54:40 -0500

    Print for CompoundStateObservationValue

--- app/src/main/java/com/philips/bleclient/observations/CompoundStateObservationValue.kt
+++ app/src/main/java/com/philips/bleclient/observations/CompoundStateObservationValue.kt
@@ -1,7 +1,12 @@
 package com.philips.bleclient.observations
 
+import com.philips.bleclient.asFormattedHexString
+
 class CompoundStateObservationValue(
-    supportedMaskBits: ByteArray,
+    val supportedMaskBits: ByteArray,
     val stateOrEventMaskBits: ByteArray,
     val bits: ByteArray): ObservationValue() {
+    override fun toString(): String {
+        return "Compound State Value with supportedMaskBits: ${supportedMaskBits.asFormattedHexString()} \nstateOrEventMaskBits: ${stateOrEventMaskBits.asFormattedHexString()} \nbits: ${bits.asFormattedHexString()}"
+    }
 }
