da7f0915641a3b58a51230084b1b6c622d7ef441
Abdul Nabi <abdul@codekrafters.com>
2022-12-07 18:12:55 -0500

    Fix UserActivity Header back button

--- app/src/main/java/com/philips/bleclient/ui/UsersActivity.kt
+++ app/src/main/java/com/philips/bleclient/ui/UsersActivity.kt
@@ -5,6 +5,8 @@ import android.os.UserManager
 import android.text.Editable
 import android.text.TextWatcher
 import android.text.method.ScrollingMovementMethod
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -52,6 +54,21 @@ class UsersActivity : AppCompatActivity() {
         // ObservationSyncer.removeListener(this)
         UsersLog.removeOnPropertyChangedCallback(logCallback)
         super.onDestroy()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return true
     }
 
     private fun setupUserIndexField() {
